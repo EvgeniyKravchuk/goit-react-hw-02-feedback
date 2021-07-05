0dd6ddf9d21639d08259f87ac118ad21dcf285ab
EvgeniyKravchuk <gekahhh1996@gmail.com>
2021-07-05 22:34:17 +0300

    Начал

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Component } from "react";
+import ButtonsData from "./componentsData/Statistics.json";
+import {
+  Wrapper,
+  Title,
+  Button,
+  Undertitle,
+  StatsList,
+  StatsItem,
+} from "./components/Statistics/Statistics.jsx";
 
-function App() {
-  return <div>Create React App</div>;
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onRateButtonClick = ({ currentTarget }) => {
+    this.setState((prevState) => ({
+      [currentTarget.id]: prevState[currentTarget.id] + 1,
+    }));
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Title>Пожалуйста, оставьте Вашу оценку:</Title>
+        {ButtonsData.map(({ id, name }) => (
+          <Button key={id} id={id} onClick={this.onRateButtonClick}>
+            {name}
+          </Button>
+        ))}
+        <Undertitle>Статистика:</Undertitle>
+        <StatsList>
+          {ButtonsData.map(({ id, name }) => (
+            <StatsItem key={id}>{name + ": " + this.state[id]}</StatsItem>
+          ))}
+        </StatsList>
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
